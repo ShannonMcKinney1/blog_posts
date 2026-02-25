0df8568787a3c1b4062a1216c1c1c85e794a1c29
Shannon McKinney <20.shannon.mckinney@gmail.com>
2026-02-25 16:40:43 -0500

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,6 @@ import icon from 'astro-icon';
 import react from '@astrojs/react';
 
 
-// Use Node adapter for local dev, Cloudflare for production
-const adapter = process.env.CF_PAGES ? cloudflare({ imageService: 'compile' }) : node({ mode: 'standalone' });
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
